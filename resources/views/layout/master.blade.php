a6502d9cd73a597a5aff62de398f7c3d36970216
mukharomdev <mukharomdev@gmail.com>
2023-05-31 14:30:51 +0700

    update:resoureces/view/layout

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -26,20 +26,45 @@
 </ul>
 </nav> -->
   <div class="container">
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-        <img class="bi me-2" width="50" height="60" src="/img/alumnismansaba2005-tinypng.png"><use xlink:href="/"/>
-        <span class="fs-8">Rekatkan Silaturahmi Eratkan Persaudaraan</span>
-      </a>
-
-      <ul class="nav nav-pills">
-        <li class="nav-item"><a href="/" class="nav-link active" aria-current="page">Home</a></li>
-        <li class="nav-item"><a href="/blog" class="nav-link">Blog</a></li>
-        <li class="nav-item"><a href="/about" class="nav-link">About</a></li>
-        <li class="nav-item"><a href="/contact" class="nav-link">Contact</a></li>
-        <li class="nav-item"><a href="/faq" class="nav-link">FAQs</a></li>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/"><img src="/img/alumnismansaba2005-tinypng.png" hight="50" width="60"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/blog">Blog</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Info
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Data Alumni</a></li>
+            <li><a class="dropdown-item" href="#">Panitia</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Donatur</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="/about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="/contact">Contact</a>
+        </li>
       </ul>
-    </header>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
   </div>
 @yield('content')
 <!-- <footer class="bg-dark py-4 text-white mt-4">
