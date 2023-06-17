f3e52665e33eac9afb1e8450a3932720e6a2a723
mukharomdev <mukharomdev@gmail.com>
2023-06-18 06:46:36 +0700

    update:resources/views/page/home.blade.php

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -2,9 +2,9 @@
 @section('title','Home')
 @section('content')
 
-<div class="container mx-xl mb-2 mt-1 ">
-<img class="img-fluid" src="{{asset('/img/brosur-smansaba.jpg')}}"  alt="sman saba" > 
-</div>
+{{-- <div class="container mx-xl mb-2 mt-1 "> --}}
+<img class="img-fluid mb-2" src="{{asset('/img/brosur-smansaba.jpg')}}"  alt="sman saba" > 
+{{-- </div> --}}
 {{-- @component('components.jumbotron')
 	@slot('jumbotron')
 		jumbotron
