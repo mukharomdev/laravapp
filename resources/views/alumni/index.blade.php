204a74609a671048ce7bae3e23f4ec90152d2b6d
mukharomdev <mukharomdev@gmail.com>
2023-06-18 10:26:18 +0700

    add:resurces/views/alumni

---
+++ resources/views/alumni/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layout.master')
+@section('title','Alumni')
+@section('content')
+
+
+<div class="container mt-3">
+ 	<div class="row">
+		<div class="col-12">
+			<div class="py-4">
+				<h2>Daftar Alumni</h2>
+			</div>
+		<table class="table table-striped">
+			<thead>
+			<tr>
+			<th>ID</th>
+			<th>NIS </th>
+			<th>NAMA</th>
+			<th>TTL</th>
+			<th>ALAMAT</th>
+			</tr>
+			</thead>
+		<tbody>
+		@forelse ($alumnies as $alumni)
+		<tr>
+		<th>{{$alumni->id}}</th>
+		<td>{{$alumni->nis}}</td>
+		<td>{{$alumni->nama}}</td>
+		<td>{{$alumni->tanggal_lahir}}</td>
+		<td>{{$alumni->alamat_lengkap}}</td>
+		</tr>
+		@empty
+		<td colspan="6" class="text-center">Tidak ada data...</td>
+		@endforelse
+		</tbody>
+		</table>
+		</div>
+	</div>
+</div>
+@endsection
