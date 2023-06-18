598ea3517ec845c5c30eee1c8f070c391724296a
mukharomdev <mukharomdev@gmail.com>
2023-06-18 11:01:07 +0700

    update:resurces/views/alumni

--- resources/views/alumni/index.blade.php
+++ resources/views/alumni/index.blade.php
@@ -3,13 +3,13 @@
 @section('content')
 
 
-<div class="container-fluid mt-3">
+<div class="container-fluid container-sm mt-3">
  	<div class="row">
 		<div class="col-12">
 			<div class="py-4">
 				<h2>Daftar Alumni</h2>
 			</div>
-		<table class="table table-striped">
+		<table class="table table-striped table-sm">
 			<thead>
 			<tr>
 			<th>ID</th>
